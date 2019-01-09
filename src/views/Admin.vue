eaa6893d02b5a5505c813a6ed12915c7d9ae1e2a
Hujjat Nazari <hujjatnazari@gmail.com>
2019-01-09 21:05:02 +1100

    Customized the Admin Menu

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -8,7 +8,7 @@
             <div class="sidebar-content">
                 <!-- sidebar-brand  -->
                 <div class="sidebar-item sidebar-brand">
-                    <a href="#">pro sidebar</a>
+                    <a href="#">Vue Shop</a>
                     <div id="close-sidebar" @click="closeMenu">
                         <i class="fas fa-times"></i>
                     </div>
@@ -16,7 +16,7 @@
                 <!-- sidebar-header  -->
                 <div class="sidebar-item sidebar-header">
                     <div class="user-pic">
-                        <img class="img-responsive img-rounded" src="img/user.jpg" alt="User picture">
+                        <img class="img-responsive img-rounded" src="/img/user.png" alt="User picture">
                     </div>
                     <div class="user-info">
                         <span class="user-name">Jhon
@@ -46,51 +46,32 @@
                 <div class=" sidebar-item sidebar-menu">
                     <ul>
                         <li class="header-menu">
-                            <span>General</span>
-                        </li>
-                        <li class="sidebar-dropdown">
-                            <a href="#">
-                                <i class="fa fa-tachometer-alt"></i>
-                                <span>Dashboard</span>
-                                <span class="badge badge-pill badge-warning">New</span>
-                            </a>
-                            <div class="sidebar-submenu">
-                                <ul>
-                                    <li>
-                                        <a href="#">Dashboard 1
-                                            <span class="badge badge-pill badge-success">Pro</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Dashboard 2</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Dashboard 3</a>
-                                    </li>
-                                </ul>
-                            </div>
+                            <span>Menu</span>
                         </li>
 
-                        <li class="header-menu">
-                            <span>Extra</span>
-                        </li>
                         <li>
                             <a href="#">
-                                <i class="fa fa-book"></i>
-                                <span>Documentation</span>
-                                <span class="badge badge-pill badge-primary">Beta</span>
+                                <i class="fa fa-chart-line"></i>
+                                <span>Overview</span>
                             </a>
                         </li>
                         <li>
                             <a href="#">
-                                <i class="fa fa-calendar"></i>
-                                <span>Calendar</span>
+                                <i class="fab fa-amazon"></i>
+                                <span>Products</span>
                             </a>
                         </li>
                         <li>
                             <a href="#">
-                                <i class="fa fa-folder"></i>
-                                <span>Examples</span>
+                                <i class="fa fa-shopping-cart"></i>
+                                <span>Orders</span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-power-off"></i>
+                                <span>Logout</span>
                             </a>
                         </li>
                     </ul>
