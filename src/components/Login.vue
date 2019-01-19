b0d29854f597093850bae9adea3078cc29c7d807
Hujjat Nazari <hujjatnazari@gmail.com>
2019-01-20 10:59:20 +1100

    firebase added

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,8 +8,6 @@
                 <div class="modal-body">
 
 
-
-
                         <ul class="nav nav-fill nav-pills mb-3" id="pills-tab" role="tablist">
                             <li class="nav-item">
                                 <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-login" role="tab" aria-controls="pills-login" aria-selected="true">Login</a>
@@ -32,19 +30,32 @@
                                 <label for="exampleInputPassword1">Password</label>
                                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                             </div>
-                        
+
+                             <div class="form-group">
+                                <button class="btn btn-primary">Login</button>
+                            </div>
+
                         </div>
                         <div class="tab-pane fade" id="pills-register" role="tabpanel" aria-labelledby="pills-register-tab">
                             
                              <h5 class="text-center">Create New Account</h5>
+                             
+                            <div class="form-group">
+                                <label for="name">Your name</label>
+                                <input type="text" class="form-control" id="name" placeholder="Your nice name">
+                            </div>
+
                             <div class="form-group">
                                 <label for="email">Email address</label>
                                 <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
-                                <small class="form-text text-muted">We'll never share your email with anyone else.</small>
                             </div>
                             <div class="form-group">
                                 <label for="password">Password</label>
                                 <input type="password" class="form-control" id="password" placeholder="Password">
+                            </div>
+
+                            <div class="form-group">
+                                <button class="btn btn-primary">Signup</button>
                             </div>
 
                         </div>
@@ -61,6 +72,7 @@
 </template>
 
 <script>
+
 export default {
   name: "Login",
   props: {
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+  
+  import firebase from 'firebase';
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDjDW8R09AFH9VifDGTZ5WSTURZnbaHURU",
+    authDomain: "vue-shop-5a95e.firebaseapp.com",
+    databaseURL: "https://vue-shop-5a95e.firebaseio.com",
+    projectId: "vue-shop-5a95e",
+    storageBucket: "vue-shop-5a95e.appspot.com",
+    messagingSenderId: "892402191551"
+  };
+  
+  firebase.initializeApp(config);
