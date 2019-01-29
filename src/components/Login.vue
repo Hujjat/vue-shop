744bae8a0efee70f0226a74d23ac8adfa2489aa2
Hujjat <Hujjatnazari@gmail.com>
2019-01-29 19:49:50 +1100

    Login Page Created

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -23,16 +23,16 @@
                             <h5 class="text-center">Login Please</h5>
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                                <input type="email" v-model="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                                 <small class="form-text text-muted">We'll never share your email with anyone else.</small>
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputPassword1">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                <input type="password" @keyup.enter="login" v-model="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                             </div>
 
                              <div class="form-group">
-                                <button class="btn btn-primary">Login</button>
+                                <button class="btn btn-primary" @click="login">Login</button>
                             </div>
 
                         </div>
@@ -89,6 +89,26 @@ export default {
   },
 
   methods:{
+      login(){
+
+          fb.auth().signInWithEmailAndPassword(this.email, this.password)
+                        .then(() => {
+                        $('#login').modal('hide')
+                          this.$router.replace('admin');  
+                        })
+                        .catch(function(error) {
+                            // Handle Errors here.
+                            var errorCode = error.code;
+                            var errorMessage = error.message;
+                            if (errorCode === 'auth/wrong-password') {
+                                alert('Wrong password.');
+                            } else {
+                                alert(errorMessage);
+                            }
+                            console.log(error);
+                    });
+
+      },
       register(){
             fb.auth().createUserWithEmailAndPassword(this.email, this.password)
                 .then((user) => {
