0d739aba718d7357233fef00bb4402995b56c6cc
Hujjat <Hujjatnazari@gmail.com>
2019-02-16 10:23:39 +1100

    Update data firetore and vuejs

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -53,7 +53,7 @@
                       <td> {{product.data().name}} </td>
                       <td> {{product.data().price}} </td>
                       <td>
-                        <button class="btn btn-primary">Edit</button>
+                        <button @click="editProduct(product)" class="btn btn-primary">Edit</button>
                         <button @click="deleteProduct(product.id)" class="btn btn-danger">Delete</button>
                       </td>
                     </tr>
@@ -65,6 +65,37 @@
 
           </div>
       </div>
+
+      <!-- Modal -->
+      <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="editLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="editLabel">Edit Product</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+
+                <div class="form-group">
+                  <input type="text" placeholder="Product Name" v-model="product.name" class="form-control">
+                </div>
+
+                <div class="form-group">
+                  <input type="text" placeholder="Price" v-model="product.price" class="form-control">
+                </div>
+
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button @click="updateProduct()" type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     
   </div>
 </template>
@@ -85,10 +116,33 @@ export default {
         product: {
           name:null,
           price:null
-        }
+        },
+        activeItem:null
     }
   },
   methods:{
+    updateProduct(){
+
+      db.collection("products").doc(this.activeItem).update(this.product)
+        .then(function() {
+          $('#edit').modal('hide');
+            console.log("Document successfully updated!");
+        })
+        .catch(function(error) {
+            // The document probably doesn't exist.
+            console.error("Error updating document: ", error);
+        });
+
+    },
+
+    editProduct(product){
+        $('#edit').modal('show');
+        this.product = product.data();
+        this.activeItem = product.id;
+
+    },
+
+
     deleteProduct(doc){
         if(confirm('Are you sure ? ')){
 
