474b630eb6421285eba8cf9dffd41182ba0c8986
Hujjat <Hujjatnazari@gmail.com>
2019-04-02 18:39:25 +1100

    Order page customized

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -5,14 +5,14 @@
         <div class="intro h-100">
             <div class="row h-100 justify-content-center align-items-center">
               <div class="col-md-6">
-                    <h3>Products Page</h3>
+                    <h3>Orders Page</h3>
                     
                  <p>
                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, ducimus.
                  </p>
               </div>
               <div class="col-md-6">
-                  <img src="/img/svg/products.svg" alt="" class="img-fluid">
+                  <img src="/img/svg/orders.svg" alt="" class="img-fluid">
               </div>
             </div>
           </div>
@@ -22,8 +22,8 @@
           <div class="product-test">
 
 
-            <h3 class="d-inline-block">Products list</h3>
-            <button @click="addNew" class="btn btn-primary float-right">Add Product</button>
+            <h3 class="d-inline-block">Orders list</h3>
+            <button @click="addNew" class="btn btn-primary float-right">Add Orders</button>
 
             <div class="table-responsive">
               
