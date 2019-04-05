5f20095b8b5c600563823adbedd6444f2be03e9f
Hujjat <Hujjatnazari@gmail.com>
2019-04-05 22:00:31 +1100

    Profile page created

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,139 +3,124 @@
       <div class="container">
           
         <div class="intro h-100">
-            <div class="row h-100 justify-content-center align-items-center">
-              <div class="col-md-6">
-                    <h3>Orders Page</h3>
+            <div class="row h-100 align-items-center">
+              <div class="col-md-6 ml-3">
+                    <h3>Profile settings</h3>
                     
                  <p>
-                   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, ducimus.
+                   Change your profile settings here
                  </p>
               </div>
-              <div class="col-md-6">
-                  <img src="/img/svg/orders.svg" alt="" class="img-fluid">
+              <div class="col-md-5">
+                  <img src="/img/svg/profile.svg" width="300" alt="" class="img-fluid">
               </div>
             </div>
           </div>
 
-          <hr>
 
-          <div class="product-test">
+          <div class="profile-content">
 
+          <ul class="nav nav-pills ml-3" id="myTab" role="tablist">
 
-            <h3 class="d-inline-block">Orders list</h3>
-            <button @click="addNew" class="btn btn-primary float-right">Add Orders</button>
+            <li class="nav-item">
+              <a class="nav-link active" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="true">Profile</a>
+            </li>
 
-            <div class="table-responsive">
-              
-                <table class="table">
-                  <thead>
-                    <tr>
-                      <th>Name</th>
-                      <th>Price</th>
-                      <th>Modify</th>
-                    </tr>
-                  </thead>
+            <li class="nav-item">
+              <a class="nav-link"  id="account-tab" data-toggle="tab" href="#account" role="tab" aria-controls="account" aria-selected="false">Account settings</a>
+            </li>
+           
+          </ul>
 
-                  <tbody>
-                      <tr v-for="product in products">
-                        <td>
-                          {{product.name}}
-                        </td>
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active pt-3" id="profile" role="tabpanel" aria-labelledby="profile-tab">
 
-                        <td>
-                          {{product.price}}
-                        </td>
-
-                        <td>
-
-                          <button class="btn btn-primary" @click="editProduct(product)">Edit</button>
-                          <button class="btn btn-danger" @click="deleteProduct(product)">Delete</button>
-                        </td>
-
-                      </tr>
-
-
-                  </tbody>
-                </table>
-            </div>
-
-          </div>
-      </div>
-
-      <!-- Modal -->
-      <div class="modal fade" id="product" tabindex="-1" role="dialog" aria-labelledby="editLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="editLabel">Edit Product</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-
-                <div class="row">
-                  <!-- main product -->
-                  <div class="col-md-8">
-                    <div class="form-group">
-                      <input type="text" placeholder="Product Name" v-model="product.name" class="form-control">
-                    </div>
-
-                    <div class="form-group">
-                      <vue-editor v-model="product.description"></vue-editor>
-                    </div>
-                  </div>
-                  <!-- product sidebar -->
-                  <div class="col-md-4">
-                    <h4 class="display-6">Product Details</h4>
-                    <hr>
-
-                    <div class="form-group">
-                      <input type="text" placeholder="Product price" v-model="product.price" class="form-control">
-                    </div>
-
-                    <div class="form-group">
-                      <input type="text" @keyup.188="addTag" placeholder="Product tags" v-model="tag" class="form-control">
-                      
-                      <div  class="d-flex">
-                        <p v-for="tag in product.tags">
-                            <span class="p-1">{{tag}}</span>
-                        </p>
-
-                      </div>
-                    </div>
-
-
-                    <div class="form-group">
-                      <label for="product_image">Product Images</label>
-                      <input type="file" @change="uploadImage" class="form-control">
-                    </div>
-
-                    <div class="form-group d-flex">
-                      <div class="p-1" v-for="(image, index) in product.images">
-                          <div class="img-wrapp">
-                              <img :src="image" alt="" width="80px">
-                              <span class="delete-img" @click="deleteImage(image,index)">X</span>
+                  <div class="container">
+                      <div class="row">
+                        
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <input type="text" name="" v-model="profile.fullName" placeholder="Full name" class="form-control">
                           </div>
-                      </div>
-                    </div>
+                        </div>
 
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <input type="text"  v-model="profile.phone" placeholder="Phone" class="form-control">
+                          </div>
+                        </div>
+
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <input type="text"  v-model="profile.address" placeholder="Address" class="form-control">
+                          </div>
+                        </div>
+
+                        <div class="col-md-8">
+                          <div class="form-group">
+                            <input type="text"  v-model="profile.postCode" placeholder="Postcode" class="form-control">
+                          </div>
+                        </div>
+
+                        <div class="col-md-4">
+                          <div class="form-group">
+                              <input type="submit" value="Save Changes" class="btn btn-primary w-100">
+                          </div>
+                        </div>
+
+                      </div>
+                  </div>
+                
+                </div>
+
+                <div class="tab-pane fade pt-3" id="account" role="tabpanel" aria-labelledby="account-tab">
+                  <div class="container">
+                      <div class="row">
+
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <input type="text"  v-model="account.name" placeholder="User name" class="form-control">
+                          </div>
+                        </div>
+
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <input type="text"  v-model="account.email" placeholder="Email address" class="form-control">
+                          </div>
+                        </div>
+
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <input type="text"  v-model="account.password" placeholder="New password" class="form-control">
+                          </div>
+                        </div>
+
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <input type="text" v-model="account.confirmPassword"  placeholder="Confirm password" class="form-control">
+                          </div>
+                        </div>
+
+                        <div class="col-md-8">
+                          <div class="form-group">
+                              <input type="file" @change="uploadImage" class="form-control">
+                           </div>
+                        </div>
+
+                        <div class="col-md-4">
+                          <div class="form-group">
+                              <input type="submit" value="Save Changes" class="btn btn-primary w-100">
+                          </div>
+                        </div>
+                      </div>
                   </div>
                 </div>
 
-
-
-
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button @click="addProduct()" type="button" class="btn btn-primary" v-if="modal == 'new'">Save changes</button>
-              <button @click="updateProduct()" type="button" class="btn btn-primary" v-if="modal == 'edit'">Apply changes</button>
-            </div>
+            
           </div>
-        </div>
-      </div>
 
+      </div>
 
     
   </div>
@@ -156,132 +141,63 @@ export default {
 
   data(){
     return {
-        products: [],
-        product: {
-          name:null,
-          description:null,
-          price:null,
-          tags:[],
-          images:[]
+        profile: {
+          fullName:null,
+          phone:null,
+          address:null,
+          postcode:null
         },
-        activeItem:null,
-        modal: null,
-        tag: null
+
+        account:{
+            name:null,
+            email:null,
+            photoUrl:null,
+            emailVerified:null,
+            password:null,
+            confirmPassword:null,
+            uid:null
+        }       
     }
   },
 
   firestore(){
       return {
-        products: db.collection('products'),
+
       }
   },
   methods:{
 
     deleteImage(img,index){
 
-      let image = fb.storage().refFromURL(img);
-
-      this.product.images.splice(index,1);
-
-      image.delete().then(function() {
-        console.log('image deleted');
-      }).catch(function(error) {
-        // Uh-oh, an error occurred!
-        console.log('an error occurred');
-      });
+    
 
     },
 
     addTag(){
-       this.product.tags.push(this.tag);
-       this.tag = "";
+     
     },
     uploadImage(e){
-
-      if(e.target.files[0]){
-        
-          let file = e.target.files[0];
-    
-          var storageRef = fb.storage().ref('products/'+ Math.random() + '_'  + file.name);
-    
-          let uploadTask  = storageRef.put(file);
-    
-          uploadTask.on('state_changed', (snapshot) => {
-            
-          }, (error) => {
-            // Handle unsuccessful uploads
-          }, () => {
-            // Handle successful uploads on complete
-            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-            
-            uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-              this.product.images.push(downloadURL);
-            });
-
-          });
-
-      }
 
 
     },
 
     reset(){
-      this.product = {
-          name:null,
-          description:null,
-          price:null,
-          tags:[],
-          images:[]
-      }
+    
     },
 
     addNew(){
-        this.modal = 'new';
-        this.reset();
-        $('#product').modal('show');
+       
     },
     updateProduct(){
-        this.$firestore.products.doc(this.product.id).update(this.product);
-          Toast.fire({
-            type: 'success',
-            title: 'Updated  successfully'
-          })
-
-           $('#product').modal('hide');
+       
     },
 
-    editProduct(product){
-      this.modal = 'edit';
-      this.product = product;
-      $('#product').modal('show');
+    editProduct(){
+     
     },
 
 
-    deleteProduct(doc){
-
-
-      Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-      }).then((result) => {
-        if (result.value) {
-
-          this.$firestore.products.doc(doc['.key']).delete()
-
-          Toast.fire({
-            type: 'success',
-            title: 'Deleted  successfully'
-          })
-
-        
-        }
-      })
-
+    deleteProduct(){
 
         
     },
@@ -292,14 +208,7 @@ export default {
     },
     addProduct(){
       
-      this.$firestore.products.add(this.product);
-      
-          Toast.fire({
-            type: 'success',
-            title: 'Product created successfully'
-          })
-
-      $('#product').modal('hide');
+    
     }
 
   
@@ -313,17 +222,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
-.img-wrapp{
-  position: relative;
-}
-.img-wrapp span.delete-img{
-    position: absolute;
-    top: -14px;
-    left: -2px;
-}
-.img-wrapp span.delete-img:hover{
-  cursor: pointer;
-}
 
 </style>
