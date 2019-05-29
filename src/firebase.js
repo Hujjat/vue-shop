4293fa25e8c171d38408785dfa2f3877da4d531c
Hujjat <Hujjatnazari@gmail.com>
2019-05-29 19:45:35 +1000

    empty firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/firebase-auth'
 import 'firebase/storage'
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyDjDW8R09AFH9VifDGTZ5WSTURZnbaHURU",
-    authDomain: "vue-shop-5a95e.firebaseapp.com",
-    databaseURL: "https://vue-shop-5a95e.firebaseio.com",
-    projectId: "vue-shop-5a95e",
-    storageBucket: "vue-shop-5a95e.appspot.com",
-    messagingSenderId: "892402191551"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
   
 const fb = firebase.initializeApp(config);
