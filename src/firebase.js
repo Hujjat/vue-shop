0f8a239967d4f4826b724a35efba14abff4a0aae
Hujjat <Hujjatnazari@gmail.com>
2019-05-29 19:44:50 +1000

    firebase updated

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import 'firebase/firebase-auth'
 import 'firebase/storage'
   // Initialize Firebase
   var config = {
-    apiKey: "z",
+    apiKey: "AIzaSyDjDW8R09AFH9VifDGTZ5WSTURZnbaHURU",
     authDomain: "vue-shop-5a95e.firebaseapp.com",
     databaseURL: "https://vue-shop-5a95e.firebaseio.com",
     projectId: "vue-shop-5a95e",
