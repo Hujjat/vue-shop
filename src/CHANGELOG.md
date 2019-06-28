fe33b1f20a459f8984da2500345502798e749837
Bantawa Pawan <rahunn3@gmail.com>
2019-06-28 14:59:27 +0545

    Features added and changelog added

---
+++ src/CHANGELOG.md
@@ -0,0 +1,10 @@
+#Changelog
+
+### Added
+- Horizontal margin to navbar buttons (login and signup)
+- Added ``` cursor:pointer; ``` to **X** on Minicart and Checkout page
+- Added increment decrement to increase and decrease product quantity
+- Added js code to handel increment and decrement of product
+
+### Bugs
+-  method ``` decrement ``` returns error when product quantity is less than 1 (Product should be removed from cart/local storage)
--- src/store.js
+++ src/store.js
@@ -41,6 +41,35 @@ export default new Vuex.Store({
 
       },
 
+      increment(state, id){
+
+        let found = state.cart.find(product => product.productId == id );
+
+        if(found){
+          found.productQuantity++;
+        }else{
+          state.cart.push(item);
+
+        }
+
+        this.commit('saveData');
+
+      },
+
+      decrement(state, id){
+
+        let found = state.cart.find(product => product.productId == id );
+
+        if(found){
+          found.productQuantity--;
+        }
+        if (found.productQuantity < 1) {
+          this.removeFromCart(state, found);          
+        }
+        this.commit('saveData');
+
+      },
+
 
       
 
