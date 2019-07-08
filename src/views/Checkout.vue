ad12190e6063baf0185a11e13f284dc26cedf7f1
Hujjat <Hujjatnazari@gmail.com>
2019-07-08 23:52:16 +1000

    Firebase Init done

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -6,7 +6,7 @@
 
             <div class="row">
 
-                <div class="col-md-9">
+                <div class="col-md-8">
                     <h4 class="py-4">Checkout page</h4>
                      <ul>
                         <li v-for="item in this.$store.state.cart" class="media">
@@ -24,17 +24,93 @@
 
                     </ul>
                 </div>
-                <div class="col-md-3">
-                    Total Price : {{ this.$store.getters.totalPrice | currency }}
+                <div class="col-md-4">
+                    <p>
+                        Total Price : {{ this.$store.getters.totalPrice | currency }}
+                    </p>
 
-                    <button class="btn btn-primary mt-4">
-                        <i class="fas fa-cart-arrow-down"></i>
-                            Proceed Checkout
-                            
-                    </button>
+                   
+
+                    <card class='stripe-card'
+                        :class='{ complete }'
+                        stripe='pk_test_XXXXXXXXXXXXXXXXXXXXXXXX'
+                        :options='stripeOptions'
+                        @change='complete = $event.complete'
+                        />
+
+                        <button class='pay-with-stripe btn btn-primary mt-4' @click='pay' :disabled='!complete'>Pay with credit card</button>
+                    
+                    </form>
                 </div>
             </div>
 
         </div>
   </div>
 </template>
+
+<script>
+
+import { Card, createToken } from 'vue-stripe-elements-plus';
+
+
+export default {
+    data () {
+    return {
+      complete: false,
+      stripeOptions: {
+        // see https://stripe.com/docs/stripe.js#element-options for details
+      }
+    }
+  },
+
+  components: { Card },
+
+  methods: {
+    pay () {
+      // createToken returns a Promise which resolves in a result object with
+      // either a token or an error key.
+      // See https://stripe.com/docs/api#tokens for the token object.
+      // See https://stripe.com/docs/api#errors for the error object.
+      // More general https://stripe.com/docs/stripe.js#stripe-create-token.
+      createToken().then(data => console.log(data.token))
+    }
+  }
+}
+</script>
+
+
+
+<style>
+/**
+ * The CSS shown here will not be introduced in the Quickstart guide, but shows
+ * how you can use CSS to style your Element's container.
+ */
+.StripeElement {
+  box-sizing: border-box;
+
+  height: 40px;
+
+  padding: 10px 12px;
+
+  border: 1px solid transparent;
+  border-radius: 4px;
+  background-color: white;
+
+  box-shadow: 0 1px 3px 0 #e6ebf1;
+  -webkit-transition: box-shadow 150ms ease;
+  transition: box-shadow 150ms ease;
+}
+
+.StripeElement--focus {
+  box-shadow: 0 1px 3px 0 #cfd7df;
+}
+
+.StripeElement--invalid {
+  border-color: #fa755a;
+}
+
+.StripeElement--webkit-autofill {
+  background-color: #fefde5 !important;
+}
+</style>
+
