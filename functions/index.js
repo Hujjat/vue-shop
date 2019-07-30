eea3a0aac91dfa854e84c5490bb642faa455dbde
Hujjat <Hujjatnazari@gmail.com>
2019-07-30 22:26:06 +1000

    73 - Stripe Checkout with Firebase Cloud Function and Vuejs

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const stripe = require("stripe")("sk_test_lOinZI9YYmU0R1M1izdHwk4000uXCMz6sG");
+const cors = require('cors')({origin: true});
+
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,6 +9,35 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
 
  response.send("Hello from Firebase!");
+ 
+});
+
+exports.CheckoutSession = functions.https.onRequest((request, response) => {
+
+
+    cors(request, response, () => {
+
+        stripe.checkout.sessions.create({
+            success_url: "https://example.com/success",
+            cancel_url: "https://example.com/cancel",
+            payment_method_types: ["card"],
+            line_items: [{
+              name: "T-shirt",
+              description: "Comfortable cotton t-shirt",
+              amount: 1500,
+              currency: "aud",
+              quantity: 2
+            }]
+          }, function(err, session) {
+               
+                response.send(session);
+          });
+    
+        
+      });
+ 
+ 
+
  
 });
 
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -29,6 +29,7 @@
                         Total Price : {{ this.$store.getters.totalPrice | currency }}
                     </p>
 
+                    <button class="btn btn-primary" @click="pay">Checkout</button>
                   
                 </div>
             </div>
@@ -39,16 +40,52 @@
 
 <script>
 
+import axios from 'axios';
+
+var stripe = Stripe('pk_test_3yyWicMGKdLdRD28kHM3ANkX00vOa4k2B3');
+
+
+
 export default {
     data () {
       return {
-      
+          sessionId:''
       }
     },
 
 
   methods: {
+
+      pay(){
+
+        stripe.redirectToCheckout({
+        // Make the id field from the Checkout Session creation API response
+        // available to this file, so you can provide it as parameter here
+        // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+        sessionId: this.sessionId.id
+        }).then(function (result) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer
+        // using `result.error.message`.
+        });
+
+      }
    
+  },
+
+  created(){
+
+      axios.post('https://us-central1-vue-shop-5a95e.cloudfunctions.net/CheckoutSession')
+        .then(response => {
+            this.sessionId = response.data
+            console.log(response.data)
+
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+
   }
 }
 </script>
