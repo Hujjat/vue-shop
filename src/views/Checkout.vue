fb533c493b75f8b6d3dde655310f0efd8b295881
Hujjat <Hujjatnazari@gmail.com>
2019-07-30 19:39:17 +1000

    Extra code removed

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -29,18 +29,7 @@
                         Total Price : {{ this.$store.getters.totalPrice | currency }}
                     </p>
 
-                   
-
-                    <card class='stripe-card'
-                        :class='{ complete }'
-                        stripe='pk_test_XXXXXXXXXXXXXXXXXXXXXXXX'
-                        :options='stripeOptions'
-                        @change='complete = $event.complete'
-                        />
-
-                        <button class='pay-with-stripe btn btn-primary mt-4' @click='pay' :disabled='!complete'>Pay with credit card</button>
-                    
-                    </form>
+                  
                 </div>
             </div>
 
@@ -50,30 +39,16 @@
 
 <script>
 
-import { Card, createToken } from 'vue-stripe-elements-plus';
-
-
 export default {
     data () {
-    return {
-      complete: false,
-      stripeOptions: {
-        // see https://stripe.com/docs/stripe.js#element-options for details
+      return {
+      
       }
-    }
-  },
+    },
 
-  components: { Card },
 
   methods: {
-    pay () {
-      // createToken returns a Promise which resolves in a result object with
-      // either a token or an error key.
-      // See https://stripe.com/docs/api#tokens for the token object.
-      // See https://stripe.com/docs/api#errors for the error object.
-      // More general https://stripe.com/docs/stripe.js#stripe-create-token.
-      createToken().then(data => console.log(data.token))
-    }
+   
   }
 }
 </script>
@@ -81,36 +56,6 @@ export default {
 
 
 <style>
-/**
- * The CSS shown here will not be introduced in the Quickstart guide, but shows
- * how you can use CSS to style your Element's container.
- */
-.StripeElement {
-  box-sizing: border-box;
 
-  height: 40px;
-
-  padding: 10px 12px;
-
-  border: 1px solid transparent;
-  border-radius: 4px;
-  background-color: white;
-
-  box-shadow: 0 1px 3px 0 #e6ebf1;
-  -webkit-transition: box-shadow 150ms ease;
-  transition: box-shadow 150ms ease;
-}
-
-.StripeElement--focus {
-  box-shadow: 0 1px 3px 0 #cfd7df;
-}
-
-.StripeElement--invalid {
-  border-color: #fa755a;
-}
-
-.StripeElement--webkit-autofill {
-  background-color: #fefde5 !important;
-}
 </style>
 
